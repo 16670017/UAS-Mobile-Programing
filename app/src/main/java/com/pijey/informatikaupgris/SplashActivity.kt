3c3cb87842b9cba76a62ea4a10c28503794865fe
pajarhidayat <pajarhidayat97@gmail.com>
2019-07-09 17:12:47 +0700

    kedua

--- app/src/main/java/com/pijey/informatikaupgris/SplashActivity.kt
+++ app/src/main/java/com/pijey/informatikaupgris/SplashActivity.kt
@@ -5,28 +5,21 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.pijey.informatikaupgris.ui.activity.DataActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
-    private var mDelayHandler: Handler? = null
-    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // sek lupa kodenya
-        //Initialize the Handler
-        mDelayHandler = Handler()
-
-        //Navigate with delay
-        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
-    }
-
-    internal val mRunnable: Runnable = Runnable {
-        if (!isFinishing) {
-
-            val intent = Intent(applicationContext, DataActivity::class.java)
+        btnInfo.setOnClickListener{
+            val intent = Intent(this,InfoActivity::class.java)
             startActivity(intent)
-            finish()
+        }
+        btnKuliah.setOnClickListener{
+            val intent = Intent(this, DataActivity::class.java)
+            startActivity(intent)
         }
     }
 }
